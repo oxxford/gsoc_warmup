de5a8a88ba7fce02940847a39c81a8f425ccdaa3
Dmitry Grigorev <gridmi007@gmail.com>
2019-04-08 19:14:02 +0300

    Task 1 fix

--- task1/stub/index.js
+++ task1/stub/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
 app.use(express.static("dist"));
 app.get("/obj", (req, res) =>
-  res.sendFile("C:\\Users\\gridm\\Desktop\\gsoc\\task1\\Pix.obj")
+  res.sendFile(path.join(__dirname, "../Pix.obj"))
 );
 app.listen(8090, () => console.log("Listening on port 8090!"));
 
